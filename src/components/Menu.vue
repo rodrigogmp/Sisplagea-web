3d85cb437849ee37c93820deb7e815f36fb072ca
matheusbfernandes <matheus.bento.fernandes@gmail.com>
2019-05-24 17:51:41 -0300

    adicionando opções no menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,48 @@
 <template>
-    <v-navigation-drawer v-model="drawer" absolute overflow clipped fixed app>
-        
+    <v-navigation-drawer width="240" dark class="grey darken-4" clipped fixed app>
+        <v-toolbar flat class="transparent">
+            <v-list class="pa-0">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                        <img src="@/assets/durelli.png">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Rafael Durelli</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-toolbar>
+
+        <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+            <v-list-tile v-for="(opcao, i) in opcoes" :to="opcao.para" :key="i" class="v-list-item">
+                <v-list-tile-action>
+                    <v-icon>{{ opcao.icone }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title v-text="opcao.titulo"></v-list-tile-title>
+            </v-list-tile>
+        </v-list>
     </v-navigation-drawer>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      opcoes: [
+        { titulo: 'Adicionar Aluno', icone: 'how_to_reg', para: '/adicionarAluno' },
+        { titulo: 'Editar Info Pessoais', icone: 'settings', para: '/infoPessoais' },
+        { titulo: 'Lista de Alunos', icone: 'supervisor_account', para: '/listarAlunos' },
+        // ['Adicionar Aluno', 'people_outline'],
+        // ['Editar Info Pessoais', 'settings'],
+        // ['Lista de Alunos', 'supervisor_account'],
+        // ['Editar Info Pessoais', 'settings'],
+      ],
+      cruds: [
+        ['Create', 'add'],
+        ['Read', 'insert_drive_file'],
+        ['Update', 'update'],
+        ['Delete', 'delete']
+      ]
+    })
+  }
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-toolbar app dark clipped-left dense fixed absolute class="light-blue darken-3">
+    <v-toolbar app dark clipped-left dense fixed class="light-blue darken-3">
         <v-toolbar-title class="headline">
             <v-icon large>business</v-icon>
             <span> SisPlagea</span>
@@ -9,8 +9,8 @@
         <v-spacer></v-spacer>
 
         <v-btn v-if="estaLogado" flat href="/home.html">
-            <span class="mr-2">Rafael Durelli</span>
-            <v-icon>home</v-icon>
+            <span class="mr-2">Sair</span>
+            <v-icon>input</v-icon>
         </v-btn>
         <v-btn v-else flat href="/login.html">
             <span class="mr-2">Login</span>
