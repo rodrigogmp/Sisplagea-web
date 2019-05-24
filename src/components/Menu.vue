8f200a0fdee3c388d6a086331a97e29665b5b8d3
matheusbfernandes <matheus.bento.fernandes@gmail.com>
2019-05-24 18:21:41 -0300

    menu pronto

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-navigation-drawer width="240" dark class="grey darken-4" clipped fixed app>
+    <v-navigation-drawer width="280" dark class="grey darken-4" clipped fixed app>
         <v-toolbar flat class="transparent">
             <v-list class="pa-0">
                 <v-list-tile avatar>
@@ -21,6 +21,19 @@
                 </v-list-tile-action>
                 <v-list-tile-title v-text="opcao.titulo"></v-list-tile-title>
             </v-list-tile>
+            <v-list-group sub-group no-action>
+                <template v-slot:activator>
+                    <v-list-tile>
+                        <v-list-tile-title>Cadastrar</v-list-tile-title>
+                    </v-list-tile>
+                </template>
+                <v-list-tile v-for="(cadastro, i) in cadastrar" :to="cadastro.para" :key="i" class="v-list-item">
+                    <v-list-tile-title v-text="cadastro.titulo"></v-list-tile-title>
+                    <v-list-tile-action>
+                        <v-icon>{{ cadastro.icone }}</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list-group>
         </v-list>
     </v-navigation-drawer>
 </template>
@@ -29,19 +42,16 @@
   export default {
     data: () => ({
       opcoes: [
-        { titulo: 'Adicionar Aluno', icone: 'how_to_reg', para: '/adicionarAluno' },
+        { titulo: 'Adicionar Aluno', icone: 'person_add', para: '/adicionarAluno' },
         { titulo: 'Editar Info Pessoais', icone: 'settings', para: '/infoPessoais' },
         { titulo: 'Lista de Alunos', icone: 'supervisor_account', para: '/listarAlunos' },
-        // ['Adicionar Aluno', 'people_outline'],
-        // ['Editar Info Pessoais', 'settings'],
-        // ['Lista de Alunos', 'supervisor_account'],
-        // ['Editar Info Pessoais', 'settings'],
+        { titulo: 'Buscar atividades', icone: 'zoom_in', para: '/listarAlunos' },
       ],
-      cruds: [
-        ['Create', 'add'],
-        ['Read', 'insert_drive_file'],
-        ['Update', 'update'],
-        ['Delete', 'delete']
+      cadastrar: [
+        { titulo: 'Publicação', icone: 'import_contacts', para: '/adicionarAluno' },
+        { titulo: 'disciplina', icone: 'border_color', para: '/adicionarAluno' },
+        { titulo: 'grupo pesq.', icone: 'group_add', para: '/adicionarAluno' },
+        { titulo: 'projeto pesq.', icone: 'attachment', para: '/adicionarAluno' },
       ]
     })
   }
