d1b9d63096b9056b64da35e0f8a0cfa9c1d14ec5
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-05-27 23:06:46 -0300

    Fazendo a tela de adicionar aluno.

--- src/components/ModuloAddAluno.vue
+++ src/components/ModuloAddAluno.vue
@@ -1,3 +1,30 @@
 <template>
-    <p>Página Adicionar Aluno</p>
+    <v-layout justify-center>
+        <v-flex xs12 sm10 md8 lg6>
+            <form>
+                <v-text-field v-model="name" :error-messages="nameErrors" :counter="254" label="Nome" required
+                @input="$v.name.$touch()"
+                @blur="$v.name.$touch()"
+                ></v-text-field>
+                <v-text-field v-model="matricula" :error-messages="matriculaErrors" label="Matricula" required
+                @input="$v.matricula.$touch()"
+                @blur="$v.matricula.$touch()"
+                ></v-text-field>
+                <v-radio-group v-model="radioGroup" label="Categoria:">
+                    <v-radio label="Iniciação Cientifica" value="1"></v-radio>
+                    <v-radio label="Mestrado" value="2"></v-radio>
+                    <v-radio label="Doutorado" value="3"></v-radio>
+                    <v-radio label="Pós-Doutorado" value="4"></v-radio>
+                </v-radio-group>
+                <v-text-field v-model="curriculo" :error-messages="curriculoErrors" 
+                label="Adicionar referencia para o curriculo lattes:" required
+                @input="$v.curriculo.$touch()"
+                @blur="$v.curriculo.$touch()"
+                ></v-text-field>
+                <v-textarea name="input-7-1" label="Adicionar informações relevantes:" 
+                v-model="informacoes" :error-messages="informacoes" required></v-textarea>
+                <v-btn @click="submit" outline color="indigo" :right="true">Adicionar</v-btn>
+            </form>
+        </v-flex>
+    </v-layout>
 </template>
