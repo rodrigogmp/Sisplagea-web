0dcc2fdf90101fc8572f490a73e21f6d840e8bba
Matheus Bento Fernandes <matheus.bento.fernandes@gmail.com>
2019-06-16 15:58:08 -0300

    melhorando login

--- src/components/ModuloLogin.vue
+++ src/components/ModuloLogin.vue
@@ -10,13 +10,14 @@
                         <v-window-item>
                             <v-card-text>
                                 <v-text-field prepend-icon="mail" label="Email" type="text" v-model="email" required></v-text-field>
-                                <v-text-field prepend-icon="lock" label="Senha" type="password" v-modal="senha"></v-text-field>
+                                <v-text-field prepend-icon="lock" label="Senha" type="password" v-modal="senha" required></v-text-field>
                                 <span class="caption grey--text text--darken-1">Informe o email e a senha.</span>
                             </v-card-text>
                         </v-window-item>
                     </v-window>
                     <v-divider></v-divider>
                     <v-card-actions>
+                        <v-btn color="secundary" outline depressed href="/paginaPrincipal.html">Voltar</v-btn>
                         <v-spacer></v-spacer>
                         <v-btn color="primary" depressed href="/home.html">Logar</v-btn>
                     </v-card-actions>
