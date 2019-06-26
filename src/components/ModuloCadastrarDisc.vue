c62b1901b33dde54901955bc3373472419b565df
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-06-26 19:27:55 -0300

    teste push

--- src/components/ModuloCadastrarDisc.vue
+++ src/components/ModuloCadastrarDisc.vue
@@ -5,19 +5,19 @@
                 <h1>Cadastrar Disciplina</h1>
                 <v-divider></v-divider>
                 <form>
-                    <v-text-field v-model="nome" :error-messages="nomeErrors" :counter="254" label="Nome" required
+                    <v-text-field v-model="nome" label="Nome" required
                     @input="$v.nome.$touch()"
                     @blur="$v.nome.$touch()"
                     ></v-text-field>
                     <v-layout row wrap>
                         <v-flex xs6 sm5 md4 lg3>
-                            <v-text-field v-model="codigo" :error-messages="codigoErrors" label="Código" required
+                            <v-text-field v-model="codigo" label="Código" required
                             @input="$v.codigo.$touch()"
                             @blur="$v.codigo.$touch()"
                             ></v-text-field>
                         </v-flex>
                         <v-flex xs6 sm5 md4 lg3>
-                            <v-text-field v-model="horas" :error-messages="horasErrors" label="Horas/Aulas" required
+                            <v-text-field v-model="horas" label="Horas/Aulas" required
                             @input="$v.horas.$touch()"
                             @blur="$v.horas.$touch()"
                             ></v-text-field>
@@ -27,12 +27,11 @@
                         <v-radio label="Graduação" value="graduate"></v-radio>
                         <v-radio label="Pós-Graduação" value="postgraduate"></v-radio>
                     </v-radio-group>
-                    <v-text-field v-model="localAulas" :error-messages="localAulasErrors" label="Local das aulas" required
+                    <v-text-field v-model="localAulas" label="Local das aulas" required
                     @input="$v.localAulas.$touch()"
                     @blur="$v.localAulas.$touch()"
                     ></v-text-field>
-                    <v-textarea name="input-7-1" label="Descrição" v-model="descricao" :error-messages="descricao" 
-                    required></v-textarea>
+                    <v-textarea label="Descrição" v-model="descricao" required></v-textarea>
                     <v-btn @click="cadastrarDisciplina" outline color="info" :right="true">Cadastrar</v-btn>
                 </form>
                 <v-alert :value="alerta" type="success" transition="scale-transition" dismissible @click="alerta = false">Disciplina cadastrada com sucesso.</v-alert>
@@ -76,10 +75,22 @@ export default {
                 }
             }).then(() => {
                 this.alerta = !this.alerta
+
+                this.nome = '',
+                this.radioGroup = '',
+                this.codigo = '',
+                this.horas = '',
+                this.localAulas = '',
+                this.descricao = ''
+
+                setTimeout(this.setAlertFalse, 5000);
                 
             }).catch(()=>{
                 this.erro = true
             })
+        },
+        setAlertFalse(){
+            this.alerta = false
         }
         
     }
