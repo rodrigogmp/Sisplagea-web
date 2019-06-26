c6dfb7f29bccd24a49a490dbd067c265720d83e3
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-06-26 19:53:54 -0300

    Adicionando botão de editar e deletar em informações.

--- src/components/ModuloListarDisciplinas.vue
+++ src/components/ModuloListarDisciplinas.vue
@@ -45,51 +45,8 @@
                                                     <v-textarea label="Abstract" v-bind:value="props.item.abstract" disabled></v-textarea>
                                                 </v-flex>
                                                 <v-spacer></v-spacer>
-                                                <v-btn outline flat rigth @click="dialog = false">Editar</v-btn>
-                                                <v-flex xs12>                                                    
-                                                    <br />
-                                                    <br />
-                                                    <br />
-                                                    <v-divider />
-                                                </v-flex>
-                                                <v-flex xs12 offset-xs3>
-                                                    <v-subheader >Adicionar o aluno a um projeto</v-subheader>
-                                                </v-flex>
-                                                <v-flex xs12>
-                                                    <v-text-field label="Nome do projeto"></v-text-field>
-                                                </v-flex>
-                                                <v-flex xs12>
-                                                    <v-textarea auto-grow rows="4" label="Resumo do projeto:"></v-textarea>
-                                                </v-flex>
-                                                <v-flex xs3>
-                                                    <v-menu ref="menuInicio" v-model="menuInicio" :close-on-content-click="false" :nudge-right="40" :return-value.sync="dataInicio" lazy transition="scale-transition" offset-y full-width min-width="290px">
-                                                        <template v-slot:activator="{ on }">
-                                                            <v-text-field v-model="dataInicio" label="Inicio" prepend-icon="event" readonly v-on="on"></v-text-field>
-                                                        </template>
-                                                        <v-date-picker v-model="dataInicio" no-title scrollable>
-                                                            <v-spacer></v-spacer>
-                                                            <v-btn flat outline @click="menuInicio = false">Cancel</v-btn>
-                                                            <v-btn flat outline color="info" @click="$refs.menuInicio.save(dataInicio)">OK</v-btn>
-                                                        </v-date-picker>
-                                                    </v-menu>
-                                                </v-flex>
-                                                <v-flex xs3>
-                                                    <v-menu ref="menuFim" v-model="menuFim" :close-on-content-click="false" :nudge-right="40" :return-value.sync="dataFim" lazy transition="scale-transition" offset-y full-width min-width="290px">
-                                                        <template v-slot:activator="{ on }">
-                                                            <v-text-field v-model="dataFim" label="Fim" prepend-icon="event" readonly v-on="on"></v-text-field>
-                                                        </template>
-                                                        <v-date-picker v-model="dataFim" no-title scrollable>
-                                                            <v-spacer></v-spacer>
-                                                            <v-btn flat outline @click="menuFim = false">Cancel</v-btn>
-                                                            <v-btn flat outline color="info" @click="$refs.menuFim.save(dataFim)">OK</v-btn>
-                                                        </v-date-picker>
-                                                    </v-menu>
-                                                </v-flex>
-                                                <v-flex xs5 offset-xs1>
-                                                    <v-card-text>
-                                                        <input type="file">
-                                                    </v-card-text>
-                                                </v-flex>
+                                                <v-btn outline flat @click="dialog= false,dialog2 = true" :right="true">Editar</v-btn>
+                                                <v-btn color="error" outline flat >Deletar</v-btn>
                                             </v-layout>
                                         </v-container>
                                     </v-card-text>
