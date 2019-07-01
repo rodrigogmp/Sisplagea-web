0a656fbad54f81dcee2c06544ead2467ae494d3c
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-07-01 12:31:25 -0300

    Verificação se existe token do user pra não permitir acessar páginas deslogado.

--- src/pages/cadastrarPublicacao/App.vue
+++ src/pages/cadastrarPublicacao/App.vue
@@ -10,6 +10,13 @@
 </template>
 
 <script>
+    var config = {
+        headers: {'access-token': localStorage.getItem("data['at']"), 'client': localStorage.getItem("data['c']"), 'content-type': localStorage.getItem("data['ct']"), 'uid': localStorage.getItem("data['uid']")}
+    }
+    if(config.headers.client == null){
+        window.location.href = '/login.html'
+    }
+    
     import Navbar from '@/components/Navbar.vue'
     import Footer from '@/components/Footer.vue'
     import Menu from '@/components/Menu.vue'
